628f05015d519ea3f0c500701d870c9d58d0b853
Adei Josol <helena.josol@bdi.ox.ac.uk>
2020-07-24 17:45:20 +0100

    Fix: 'CompileStatic' - Explicitly annotate class with @CompileDynamic

--- src/main/groovy/uk/ac/ox/softeng/maurodatamapper/plugin/database/postgres/PostgresDatabasePlugin.groovy
+++ src/main/groovy/uk/ac/ox/softeng/maurodatamapper/plugin/database/postgres/PostgresDatabasePlugin.groovy
@@ -19,7 +19,10 @@ package uk.ac.ox.softeng.maurodatamapper.plugin.database.postgres
 
 import uk.ac.ox.softeng.maurodatamapper.provider.plugin.AbstractMauroDataMapperPlugin
 
+import groovy.transform.CompileDynamic
+
 // @CompileStatic
+@CompileDynamic
 class PostgresDatabasePlugin extends AbstractMauroDataMapperPlugin {
 
     @Override
