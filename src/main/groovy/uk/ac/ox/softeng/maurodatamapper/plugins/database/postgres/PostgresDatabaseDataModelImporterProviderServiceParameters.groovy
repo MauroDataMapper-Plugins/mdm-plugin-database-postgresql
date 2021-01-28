73d36557cdbbfaaa00cf97f5a45f8c69c55db813
Ollie Freeman <oliver.freeman@bdi.ox.ac.uk>
2021-01-28 15:52:24 +0000

    Update LICENSE and NOTICE with the copyright to include NHSD

--- src/main/groovy/uk/ac/ox/softeng/maurodatamapper/plugins/database/postgres/PostgresDatabaseDataModelImporterProviderServiceParameters.groovy
+++ src/main/groovy/uk/ac/ox/softeng/maurodatamapper/plugins/database/postgres/PostgresDatabaseDataModelImporterProviderServiceParameters.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 University of Oxford
+ * Copyright 2020 University of Oxford and Health and Social Care Information Centre, also known as NHS Digital
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,9 +21,8 @@ import uk.ac.ox.softeng.maurodatamapper.core.provider.importer.parameter.config.
 import uk.ac.ox.softeng.maurodatamapper.core.provider.importer.parameter.config.ImportParameterConfig
 import uk.ac.ox.softeng.maurodatamapper.plugins.database.DatabaseDataModelImporterProviderServiceParameters
 
-import org.postgresql.ds.PGSimpleDataSource
-
 import groovy.util.logging.Slf4j
+import org.postgresql.ds.PGSimpleDataSource
 
 @Slf4j
 // @CompileStatic
